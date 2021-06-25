22776757d75397d59bec37172b51293c0b21e30f
ShubhhGupta7 <71902970+ShubhhGupta7@users.noreply.github.com>
2021-06-25 11:50:43 +0530

    add views to to home, profile and posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 module.exports.home  = function(req, res) {
 
-    return res.end('<h1> Express is up for our codial! </h1>');
+    return res.render('home', {
+        'title' : 'Home'
+    });
 }
 
 // module.exports.action_name = function(req, res) {}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 module.exports.post = function(req, res) {
 
-    return res.end('<h1> Users Post! </h1>');
+   return res.render('post', {
+       title : 'Posts'
+   });
 }
