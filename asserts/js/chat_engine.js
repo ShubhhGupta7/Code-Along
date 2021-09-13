81ce529ccf856aba4361f68312d7812750750730
ShubhhGupta7 <shubhgupta036@gmail.com>
2021-09-13 15:17:35 +0530

    add devlopment 2

--- asserts/js/chat_engine.js
+++ asserts/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userId = userId;
 
-        this.socket = io.connect('http://18.216.116.156:5000');
+        this.socket = io.connect('http://3.82.201.131:10000/');
 
         self = this;
         if (this.userId){
